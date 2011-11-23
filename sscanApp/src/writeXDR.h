ab401453e7936619f25a0965fa47d02b9ac208b2
timmmooney <mooney@aps.anl.gov>
2011-11-23 21:31:51 +0000

    Include epicsTypes.h, and eplace long with epicsInt32 in writeXDR_long() arg list

--- sscanApp/src/writeXDR.h
+++ sscanApp/src/writeXDR.h
@@ -1,5 +1,6 @@
 #include <sys/types.h>
 #include <stdio.h>
+#include <epicsTypes.h>
 
 typedef int (*xdrproc_t)();
 
@@ -7,7 +8,7 @@ extern int write_XDR_Init();
 extern int writeXDR_char(FILE *fd, char *cp);
 extern int writeXDR_short(FILE *fd, short *sp);
 extern int writeXDR_int(FILE *fd, int *ip);
-extern int writeXDR_long(FILE *fd, long *lp);
+extern int writeXDR_long(FILE *fd, epicsInt32 *lp);
 extern int writeXDR_float(FILE *fd, float *fp);
 extern int writeXDR_double(FILE *fd, double *dp);
 extern int writeXDR_counted_string(FILE *fd, char **p);
